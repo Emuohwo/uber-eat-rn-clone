47c0e7d8fee5e80340c30cb9da191b966c9f70a5
Emuohwo <warriisaac@outlook.com>
2021-11-16 09:37:39 -1100

    Add bottom tabs to home page

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import { View, Text, SafeAreaView, ScrollView } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Divider } from 'react-native-elements'
+import BottomTabs from '../components/BottomTabs'
 import Categories from '../components/Categories'
 import HeaderTabs from '../components/HeaderTabs'
 import RestaurantItems, { localRestaurants } from '../components/RestaurantItems'
@@ -50,6 +53,8 @@ export default function Home() {
                   restaurantData={restaurantData}
                 />
             </ScrollView>
+            <Divider width={1} />
+            <BottomTabs />
         </SafeAreaView>
     )
 }
